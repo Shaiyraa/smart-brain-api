8c0648f5cf12b61b8a47e8a80c6331f83d185077
Karina Kuss <karina.kuss00@gmail.com>
2020-12-13 07:33:23 +0100

    dockerized the backend

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   // connect to your own database here
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
